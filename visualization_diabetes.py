74c07e4a35f86558d2f6006be19771f4bf3d4c8a
Kinga <zygmuntowicz.kin@gmail.com>
2025-04-01 08:24:06 -0400

    Update visualization_diabetes.py

--- visualization_diabetes.py
+++ visualization_diabetes.py
@@ -1,13 +1,12 @@
 import pandas as pd
 import plotly.express as px
 
-# Load the processed data
-df = pd.read_csv('processed_diabetes_data.csv')
+df = pd.read_csv('processed_diabetes_data_zyg.csv')
 
 # Visualizing the distribution of the target variable (Outcome)
-fig = px.bar(df, x='Outcome', title="Distribution of Outcome Variable")
-fig.show()
+vis = px.bar(df, x='Outcome', title="Distribution of Outcome Variable")
+vis.show()
 
-# Visualizing correlation matrix
+# Matrix Correlation
 fig_corr = px.imshow(df.corr(), text_auto=True, title="Correlation Matrix")
 fig_corr.show()
