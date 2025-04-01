4a14e357a9fd9d83d4b9e0188a4650f12f899091
Kinga <zygmuntowicz.kin@gmail.com>
2025-04-01 10:43:41 -0400

    Update data_processing_diabetes.py

--- data_processing_diabetes.py
+++ data_processing_diabetes.py
@@ -4,13 +4,18 @@ import pandas as pd
 from sklearn.impute import SimpleImputer 
 from sklearn.preprocessing import LabelEncoder
 
-file_path = 'diabetes_data_zyg.csv'
-df = pd.read_csv(file_path)
+# I downloaded date from website pima-indians-diabetes
+data_url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
+columns = ["Pregnancies", "Glucose", "BloodPressure", "SkinThickness", "Insulin", 
+           "BMI", "DiabetesPedigreeFunction", "Age", "Outcome"]
+
+df = pd.read_csv(data_url, names=columns)
+
+df.to_csv('diabetes_data_zyg.csv', index=False)
 
 imputer = SimpleImputer(strategy='mean')
 num_cols = df.select_dtypes(include=['float64', 'int64']).columns
 df[num_cols] = imputer.fit_transform(df[num_cols])
-
 
 label_encoders = {}
 cat_cols = df.select_dtypes(include=['object']).columns
@@ -20,4 +25,3 @@ for col in cat_cols:
     label_encoders[col] = le
 
 df.to_csv('processed_data_diabetes_zyg.csv', index=False)
-
